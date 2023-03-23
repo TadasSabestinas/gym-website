c13a5d7eb99e193f9d8026c87d7c2c5852098dc9
TadasSabestinas <90024877+TadasSabestinas@users.noreply.github.com>
2023-03-23 17:14:37 +0200

    adding reveal effect
    reveal effect added once reached to a certain section of the page

--- index.js
+++ index.js
@@ -43,6 +43,27 @@ var swiper = new Swiper(".home-slider", {
     disableOnInteraction: false,
   },
 });
+
+// for reveal effect
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i=0; i< reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active')
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
 //images/gym-equipment.jpeg
 //images/cardio-equipment.jpeg
 //images/cardio-equipment.jpeg
